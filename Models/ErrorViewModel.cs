11f1fbcaa32d2f2e0cca8122a03e0e81ab5dd321
gregor suttie <gsuttie2001@hotmail.com>
2021-05-02 21:57:17 +0100

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace GolfStats.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
